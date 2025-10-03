7319349bd028cc68b7b9809cc200a97027c26a08
Rohit Shinde <rohit.shinde12194@gmail.com>
2025-10-03 14:27:05 +0000

    Added basic dashboard

---
+++ utils/constants.py
@@ -0,0 +1,61 @@
+indian_states = [
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal"
+]
+
+indian_state_initials = [
+    "AP",  # Andhra Pradesh
+    "AR",  # Arunachal Pradesh
+    "AS",  # Assam
+    "BR",  # Bihar
+    "CG",  # Chhattisgarh
+    "GA",  # Goa
+    "GJ",  # Gujarat
+    "HR",  # Haryana
+    "HP",  # Himachal Pradesh
+    "JH",  # Jharkhand
+    "KA",  # Karnataka
+    "KL",  # Kerala
+    "MP",  # Madhya Pradesh
+    "MH",  # Maharashtra
+    "MN",  # Manipur
+    "ML",  # Meghalaya
+    "MZ",  # Mizoram
+    "NL",  # Nagaland
+    "OR",  # Odisha
+    "PB",  # Punjab
+    "RJ",  # Rajasthan
+    "SK",  # Sikkim
+    "TN",  # Tamil Nadu
+    "TS",  # Telangana
+    "TR",  # Tripura
+    "UP",  # Uttar Pradesh
+    "UK",  # Uttarakhand
+    "WB"   # West Bengal
+]
