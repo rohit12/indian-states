3cac30eb1b599062b308e7ba2dc9956fd41f8f10
Rohit Shinde <rohit.shinde12194@gmail.com>
2025-10-11 02:46:46 +0000

    Added page 3 - revex and capex

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+from utils.constants import indian_states, state_colors1
+import plotly.express as px
 st.set_page_config(page_title="Indian States Dashboard", layout="wide")
 
 st.title("🏛️ Indian States Dashboard")
@@ -10,3 +11,6 @@ Use the sidebar to explore different statistics:
 
 - 📊 State Revenue Receipts
 """)
+
+color_map = {state: state_colors1[i % len(state_colors1)] for i, state in enumerate(indian_states)}
+px.defaults.color_discrete_map = color_map
--- utils/constants.py
+++ utils/constants.py
@@ -12,6 +12,15 @@ indian_state_initials = [
     "RJ", "SK", "TN", "TS", "TR", "UP", "UK", "WB"
 ]
 
+state_colors1 = [
+    "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
+    "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
+    "#aec7e8", "#ffbb78", "#98df8a", "#ff9896", "#c5b0d5",
+    "#c49c94", "#f7b6d2", "#c7c7c7", "#dbdb8d", "#9edae5",
+    "#393b79", "#637939", "#8c6d31", "#843c39", "#7b4173",
+    "#17becf", "#b5cf6b", "#e7ba52"
+]
+
 # Map state -> initials
 state_to_initial = dict(zip(indian_states, indian_state_initials))
 
